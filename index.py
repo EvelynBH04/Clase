f76459288daf1eafaf4df58e18c4b482131fb6b2
oscarparra0 <oscarivan.parraflorian@gmail.com>
2022-02-15 10:54:48 -0500

    modificacion index

--- index.py
+++ index.py
@@ -1,8 +1,15 @@
 
-import aritmetica.py as a
+import aritmetica as a
 import text as t
 
 print(a.sumDosNum(2,-3))
 print(a.restDosNum(1,-2))
-print(a.multDosNum(5*5))
+print(a.multDosNum(5,5))
+print(a.divDosNum(100,2))
+print(a.potencia(20,5))
+print(a.rad(70))
+print(a.log(76))
+print(a.sin(45))
+print(a.cos(59))
+print(a.tang(66))
 print(t.concat("Hola","Mundo"))
