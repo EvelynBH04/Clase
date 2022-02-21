70b062829aa4f0ae97fbb4163328264675369811
Evelyn Buitrago <ebuitragohe@uninpahu.edu.co>
2022-02-21 10:52:18 -0500

    .

--- aritmetica.py
+++ aritmetica.py
@@ -1,8 +1,26 @@
-class Operation:
-    def sumDosNum(num1,num2):
-        return num1+num2
-    def restDosNum(num1,num2):
-        return num1-num2
-    def multDosNum(num1,num2):
-        return num1*num2
-    
+from cmath import pi
+import math
+
+Pi=math.pi
+e=math.e
+PHI=1.16e-4
+G=6.67e-11
+
+def Sum(N1,N2):
+     return N1+N2
+def Rest(N1,N2):
+    return N1-N2
+def Mult(N1,N2):
+    return N1*N2
+def Div(N1,N2):
+    return N1/N2
+def Pot(N1,N2):
+    return N1**N2
+def Rad(N):
+    return math.sqrt(N)
+def Tan(N):
+    return math.tan(N)
+def Sin(N):
+    return math.sin(N)
+def Cos(N):
+    return math.cos(N)
