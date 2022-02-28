95872432337fc664d2c9b72189b228d6c88facfc
erikavanessa18 <erikavanessamartinlopez18@gmail.com>
2022-02-28 10:02:49 -0500

    Se repara el conflicto

--- text.py
+++ text.py
@@ -1,3 +1,14 @@
+['NOMBRE Y APELLIDO DE HIJOS:', 'MARIANA ROSALES YESENIA ROSALES']
+
+dato_personal[1]
+
+
+posicion_de_corte = dato_personal[1].replace(" ", 'X', 2).find(" ")
+primer_dato = dato_personal[1][:posicion_de_corte]
+primer_dato_clean = primer_dato.strip()
+
+ef concat(tex1,tex2):
+    return tex1+tex2 
 texto = 'Hola mundo'
 print (texto)
 
@@ -7,8 +18,26 @@ linea.strip()
 
 adn.lower().replace(' ', '')
 acttgacta
+print (("Mi Nombre Es"," Pepe")))
+print("Hola ","Mundo"))
+print("Buenas ","tardes"))
+print("Feliz "," Dia"))
+print("Mi Nombre Es","Pepe"))
 
 
 
 
-#modificado
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
